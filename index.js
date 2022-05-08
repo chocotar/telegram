17d9ea6c99a4a5789708dcaebe74308c1c8a416b
chocotar <80971016+chocotar@users.noreply.github.com>
2022-05-08 15:56:00 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const token = process.env.TOKEN
+const PORT = process.env.PORT || 8000
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+const opts = {
+  reply_markup:{
+    inline_keyboard: [ ['FAQ'], ['Buy'] ] // add keyboard response text
+  },
+  parse_mode: 'HTML'
+};
+const app = express()
+
+// Matches "/find [whatever]"
+bot.onText(/\/find (.+)/, (msg, match) => {
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  bot.sendMessage(chatId, `<b>Finding:</b> <i>${resp}</i>`, opts);
+});
+
+bot.onText(/\/scrape (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1]
+
+  bot.sendMessage(chatId, `<b>Scraping:</b> <i>${resp}</i>`, opts);
+});
+
+bot.on('polling_error', (error) => {
+  console.log(error);  // => 'EFATAL'
+});
+
+// Error  handling
+bot.on('error', (error) => {
+  console.log(error.code);  // => 'EFATAL'
+});
+// Server
+app.get('/', (req, res) => {
+  res.send("It's Running")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`)
+})
