c5cdac690284525529c3cae7ab0b9a0ee24a77d5
chocotar <80971016+chocotar@users.noreply.github.com>
2022-05-08 17:10:04 +0700

    Update promiseHandler.js

--- promiseHandler.js
+++ promiseHandler.js
@@ -1,8 +1,8 @@
 const inlineKeyboard = (text, callback) => {
   const query = text.map( (e, i) => {
       return JSON.stringify({
-        'text': e,
-        'callback_data': callback[i]
+        text: e,
+        callback_data: callback[i]
       })
   })
   return query
