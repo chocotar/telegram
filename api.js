e9a3ae8c9cd50e1c79137e874970e86105afb843
chocotar <80971016+chocotar@users.noreply.github.com>
2022-05-10 12:08:11 +0700

    Add files via upload

--- api.js
+++ api.js
@@ -1,13 +1,25 @@
+const https = require('node:https');
 const axios = require('axios');
-const withDNS = require('axios-with-dns');
+const cheerio = require('cheerio');
 
-withDNS(axios)
 const agent = new https.Agent({  
   rejectUnauthorized: false
 });
 const getLink = async url => {
-  const promise = await axios.get(url, {dnsServer: '1.1.1.1', httpsAgent: agent})
+  const promise = await axios.get(url, {httpsAgent: agent})
   return promise
 }
+
+const req = getLink('https://mrcong.com/tag/djawa/page/1/')
+
+req
+  .then(
+    response => {
+      const html = response.data
+      const $ = cheerio.load(html)
+      const name = $('div.gs-title a.gs-title').text()
+      console.log(name)
+    }
+  )
 
 module.exports.getLink = getLink
