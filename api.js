d44fb2c6f0503311894e67e8d94e2b04971cf536
chocotar <80971016+chocotar@users.noreply.github.com>
2022-05-10 12:07:08 +0700

    Add files via upload

---
+++ api.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+const withDNS = require('axios-with-dns');
+
+withDNS(axios)
+const agent = new https.Agent({  
+  rejectUnauthorized: false
+});
+const getLink = async url => {
+  const promise = await axios.get(url, {dnsServer: '1.1.1.1', httpsAgent: agent})
+  return promise
+}
+
+module.exports.getLink = getLink
