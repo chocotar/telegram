210554f2e2bdb53604d1a1bd8e349fafdcd47e62
chocotar <80971016+chocotar@users.noreply.github.com>
2022-05-15 23:54:18 +0700

    Delete promiseHandler.js

--- promiseHandler.js
+++
@@ -1,7 +0,0 @@
-const inlineKeyboard = (text) => {
-  return text.map( (e) => e.split(','))
-}
-
-const a = ["YouMi Vol.753: Zhu Ke Er (朱可儿Flora) (86 ảnh)", "XIUREN No.4272: Zhu Ke Er (朱可儿Flora) (58 ảnh)", "XIUREN No.4402: 李雅柔182CM (64 ảnh)"]
-
-console.log(inlineKeyboard(a))
