925e457e868bf8529f2acdcaae252e309bbcaeb0
chocotar <80971016+chocotar@users.noreply.github.com>
2022-05-15 13:05:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -57,11 +57,13 @@ bot.on('callback_query', callbackQuery => {
   const keyboardBuild = inlineKeyboardBuilder(data, index)
   const options = opts(true, keyboardBuild[1])
 
+  bot.deleteMessage(chatId, botMsg)
+
   if (query == index) {
-    return bot.editMessageText(keyboardBuild[0], chatId, options)
+    bot.sendMessage(keyboardBuild[0], chatId, options)
+    return
   }
 
-  bot.deleteMessage(chatId, botMsg)
   getLink(data[query].link)
     .then(scrapePromiseHandler(bot, chatId, null, data[query].link))
     .catch(errorHandler(bot, chatId))
