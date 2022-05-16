eeb1c87720fa7decd3af682189db5a4e1e3929bc
chocotar <80971016+chocotar@users.noreply.github.com>
2022-05-16 10:43:32 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-const URI = process.env.URI || 'mongodb+srv://rezcoco:<Zaki1234>@cluster0.tg2sf.mongodb.net/mrcong?retryWrites=true&w=majority'
+const URI = process.env.URI || 'mongodb+srv://rezcoco:Zaki1234@cluster0.tg2sf.mongodb.net/mrcong?retryWrites=true&w=majority'
 
 mongoose.connect(URI).then( () => console.log('Success to connect')).catch((err) => console.log(err))
