da364eec58f32fc3b2f6a6cf87a23d20e10734bc
chocotar <80971016+chocotar@users.noreply.github.com>
2022-05-16 09:12:19 +0700

    Update db.js

--- db.js
+++ db.js
@@ -4,11 +4,17 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 const insertData = (obj) => {
   client.connect(err => {
-  if (err) console.log(err)
+  if (err) {
+    return console.log(err)
+  }
   const collection = client.db("mrcong").collection("links");
   // perform actions on the collection object
- const insertResult = collection.insertOne(obj)
- console.log('Inserted data =>', insertResult)
+  collection.insertOne(obj, (err, result) => {
+    if (err) {
+      return console.log(err)
+    }
+    console.log('Inserted data =>', result)
+  })
   client.close();
   });
 }
