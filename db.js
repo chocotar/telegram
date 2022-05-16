8fc6b910b7c5ea6ed4a84814da4b9962a6d3feb2
chocotar <80971016+chocotar@users.noreply.github.com>
2022-05-16 10:36:13 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,22 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://rezcoco:Zaki1234@cluster0.tg2sf.mongodb.net/mrcong?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1, connectTimeoutMS: 1000 });
+const mongoose = require('mongoose');
+const URI = process.env.URI || 'mongodb+srv://rezcoco:<Zaki1234>@cluster0.tg2sf.mongodb.net/mrcong?retryWrites=true&w=majority'
 
-const insertData = (obj) => {
-  client.connect(err => {
-  if (err) {
-    return console.log(err)
-  }
-  const collection = client.db("mrcong").collection("links");
-  // perform actions on the collection object
-  collection.insertOne(obj, (err, result) => {
-    if (err) {
-      return console.log(err)
-    }
-    console.log('Inserted data =>', result)
-  })
-  client.close();
-  });
-}
-
-module.exports.insertData = insertData
+await mongoose.connect(URI).then( () => console.log('Success to connect')).catch((err) => console.log(err))
