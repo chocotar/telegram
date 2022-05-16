1127279dfbb0d11030ab57b64eda35800193ad61
chocotar <80971016+chocotar@users.noreply.github.com>
2022-05-16 09:02:53 +0700

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://rezcoco:Zaki1234@cluster0.tg2sf.mongodb.net/mrcong?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
