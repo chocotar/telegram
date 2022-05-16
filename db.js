e0a9263fb102a784d52f8f3d2e35d1f91375d094
chocotar <80971016+chocotar@users.noreply.github.com>
2022-05-16 12:45:47 +0700

    Update db.js

--- db.js
+++ db.js
@@ -5,25 +5,31 @@ const insertData = obj => {
   mongoose.connect(URI, { 
     useNewUrlParser: true,
     useUnifiedTopology: true
-  }).then(() => {
-    console.log('Connected to Mongodb')
-    const Schema = mongoose.Schema
-
-    const LinkSchema = new Schema({ 
-      name: { type: String, index: true },
-      link: Schema.Types.Mixed 
-    })
-    const Link = mongoose.model('link', LinkSchema)
-    const linkDownload = new Link(obj)
-    linkDownload.save((err, result) => {
-      if (err) {
-        console.log(err)
-        return mongoose.disconnect()
-      }
-      console.log(result)
-      return mongoose.disconnect()
-    })
+  }).then(() => { console.log('Connected to Mongodb')
   }).catch((err) => console.log(err))
+
+  (async () => {
+    try {
+      const Schema = mongoose.Schema
+
+      const LinkSchema = await new Schema({ 
+        name: { type: String, index: true },
+        link: Schema.Types.Mixed 
+      })
+      const Link = await mongoose.model('link', LinkSchema)
+      const linkDownload = await new Link(obj)
+      await linkDownload.save((err, result) => {
+        if (err) {
+          console.log(err)
+          return mongoose.disconnect()
+        }
+        console.log(result)
+        return mongoose.disconnect()
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  })();
   mongoose.disconnect()
 }
 
