034b2eadd64a2c4abad4c651408b6d337164f773
chocotar <80971016+chocotar@users.noreply.github.com>
2022-05-16 11:39:57 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,24 @@
 const mongoose = require('mongoose');
-const URI = process.env.URI || 'mongodb+srv://rezcoco:Zaki1234@cluster0.tg2sf.mongodb.net/mrcong?retryWrites=true&w=majority'
+const URI = process.env.URI
 
-mongoose.connect(URI).then( () => console.log('Success to connect')).catch((err) => console.log(err))
+const insertData = obj => {
+  mongoose.connect(URI).then( () => console.log('Connected to Mongodb')).catch((err) => console.log(err))
+  const Schema = mongoose.Schema
+
+  const LinkSchema = new Schema({ 
+    name: String, 
+    link: Schema.types.mixed 
+  })
+  const Link = mongoose.model('link', LinkSchema)
+  const linkDownload = new Link(obj)
+  linkDownload.save((err, result) => {
+    if (err) {
+      return console.log(err)
+    }
+    console.log(result)
+    mongoose.connection.close()
+    return
+  })
+}
+
+module.exports.insertData = insertData
