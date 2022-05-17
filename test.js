5372983cd0748398c7eaf7e4038ad4ac2ff2e0be
chocotar <80971016+chocotar@users.noreply.github.com>
2022-05-17 20:18:38 +0700

    Update test.js

--- test.js
+++ test.js
@@ -5,10 +5,9 @@ const cheerio = require('cheerio');
 const agent = new https.Agent({  
   rejectUnauthorized: false
 });
+const res = axios.get(url, {httpsAgent: agent})
 const getLink = async url => {
-  const res = await axios.get(url, {httpsAgent: agent})
-  
-  const $ = cheerio.load(res)
+  const $ = await cheerio.load(res)
   const element = $('.post-box-title')
   console.log(element)
 }
