4f237dffe9cf20dec8d1131621c63d2897e7de86
chocotar <80971016+chocotar@users.noreply.github.com>
2022-05-17 20:28:59 +0700

    Update test.js

--- test.js
+++ test.js
@@ -5,12 +5,16 @@ const cheerio = require('cheerio');
 const agent = new https.Agent({  
   rejectUnauthorized: false
 });
-const url = 'https://mrcong.com'
-const res = axios.get(url, {httpsAgent: agent})
-const getLink = async () => {
-  const $ = await cheerio.load(res)
-  const element = $('.post-box-title')
-  console.log($)
+const getLink = async url => {
+  try {
+    const res = await axios.get(url, {httpsAgent: agent})
+  
+    const $ = cheerio.load(res)
+    const element = $('.post-box-title')
+    console.log(res)
+  } catch(err) {
+    console.log(err)
+  }
 }
 
-getLink()
+getLink('https://mrcong.com')
