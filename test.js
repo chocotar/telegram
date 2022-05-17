b5c0866b606eee3ab264a8db045c9890c3dd1b52
chocotar <80971016+chocotar@users.noreply.github.com>
2022-05-17 19:30:36 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,16 @@
+const https = require('node:https');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const agent = new https.Agent({  
+  rejectUnauthorized: false
+});
+const getLink = async url => {
+  const res = await axios.get(url, {httpsAgent: agent})
+  
+  const $ = await cheerio.load(res)
+  const element = $('.post-box-title')
+  console.log(element)
+}
+
+getLink('https://mrcong.com')
