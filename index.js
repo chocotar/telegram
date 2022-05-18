4655ee0c6ed61f787bf1bd54ab4243d94552aa10
chocotar <80971016+chocotar@users.noreply.github.com>
2022-05-18 21:59:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -59,15 +59,16 @@ bot.on('callback_query', callbackQuery => {
   const { message_id } = callbackQuery.message
   const query = callbackQuery.data
   const { data, nextIndex } = dataUrl
-  const { reply_markup, parse_mode } = opts(true, keyboardBuild[1])
 
   // Next button
   if (query == nextIndex) {
     const keyboardBuild = inlineKeyboardBuilder(data, nextIndex)
+    const { reply_markup, parse_mode } = opts(true, keyboardBuild[1])
     bot.editMessageText(keyboardBuild[0], { chat_id: chatId, message_id, reply_markup, parse_mode })
-
+    
   } else if (query == 'prev') { // prev button
     const keyboardBuild = inlineKeyboardBuilder(data, nextIndex - 10)
+    const { reply_markup, parse_mode } = opts(true, keyboardBuild[1])
     bot.editMessageText(keyboardBuild[0], { chat_id: chatId, message_id, reply_markup, parse_mode })
 
   } else if (query == 'nextPage') {
