fa348ddbff5f2a3239b9e5b9ba36e0ec0c62e627
chocotar <80971016+chocotar@users.noreply.github.com>
2022-05-20 22:51:37 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -117,10 +117,10 @@ const grabber = async (bot, chatId, botMsg, baseUrl, page) => {
         }
       }
     if (!dataUrl.msg) {
-      dataUrl.msg = bot.editMessageText(`<i>${totalGrabbed}</i><b>Data grabbed from page: </b><i>${pageNum}</i>`, { chat_id: chatId, message_id, parse_mode: 'HTML' })
+      dataUrl.msg = bot.editMessageText(`<i>${totalGrabbed}</i><b> Data grabbed from page: </b><i>${pageNum}</i>`, { chat_id: chatId, message_id, parse_mode: 'HTML' })
     } else {
       var { message_id } = dataUrl.msg
-      dataUrl.msg = bot.editMessageText(`<i>${totalGrabbed}</i> <b>Grabbed From Page: </b><i>${pageNum}</i>`, { chat_id: chatId, message_id, parse_mode: 'HTML' })
+      dataUrl.msg = bot.editMessageText(`<i>${totalGrabbed}</i><b> Data grabbed from page: </b><i>${pageNum}</i>`, { chat_id: chatId, message_id, parse_mode: 'HTML' })
     }
     pageNum++
     }
