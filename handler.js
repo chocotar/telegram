959ec1ac055cd8d68e31cccca5aef1224b6bd7fb
chocotar <80971016+chocotar@users.noreply.github.com>
2022-05-22 00:04:36 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -146,7 +146,8 @@ const tagSearch = async url => {
     const arr = []
     element.each((index, el) => {
       if ($(el).text() && $(el).attr('href')) {
-        arr[index] = { name: $(el).text(), link: $(el).attribs.href }
+        console.log($(el))
+        arr[index] = { name: $(el).text(), link: $(el).attr('href') }
       }
     })
     return arr 
