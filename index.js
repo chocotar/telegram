6ddaabbbe89aa32af94f8577627fb0a0266832c5
chocotar <80971016+chocotar@users.noreply.github.com>
2022-05-21 23:22:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const grabberHandler = async (msg, match) => {
     const resp = match[1];
     const args = resp.split(' page: ')
     const query = args[0]
-    const page = (args[1] == 'end') ? 'end' : Number(args[1])
+    const page = args[1] //Number(args[1])
     console.log(query, page)
     
     const botMsg = await bot.sendMessage(chatId, `<b>Grabbing:</b> ${query}`, htmlParse)
